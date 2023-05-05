ebbce8640157086ce22296681c2cdfbcee977105
Gabriel Sene <sene300@gmail.com>
2023-05-05 11:20:11 -0300

    feat: Criação do filtro, recuperação e validação do tokenJWT

---
+++ src/main/java/br/com/fut/futregistry/configuration/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package br.com.fut.futregistry.configuration.security;
+
+public record DadosTokenJWT(String tokenJWT) {
+}
--- src/main/java/br/com/fut/futregistry/controller/AutenticacaoController.java
+++ src/main/java/br/com/fut/futregistry/controller/AutenticacaoController.java
@@ -1,14 +1,17 @@
 package br.com.fut.futregistry.controller;
 
+import br.com.fut.futregistry.configuration.security.DadosTokenJWT;
 import br.com.fut.futregistry.configuration.security.TokenService;
 import br.com.fut.futregistry.domain.usuario.DadosAutenticacao;
 import br.com.fut.futregistry.domain.usuario.Usuario;
+import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,11 +25,12 @@ public class AutenticacaoController {
     private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity efetuarLogin(DadosAutenticacao dados){
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
         var authenticationToken = new UsernamePasswordAuthenticationToken(dados.user(), dados.password());
         var authentication = authenticationManager.authenticate(authenticationToken);
         var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
-        return ResponseEntity.ok(tokenJWT);
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+
 
     }
 }
